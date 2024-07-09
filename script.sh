92ca47dc74af4c24c5cca9ff34338128848df32e
Ahmed Samir <134155493+ahmd7@users.noreply.github.com>
2024-07-09 19:04:52 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,14 @@
-I'm not a script
+#!/bin/bash
+
+
+directory=$1
+
+for item in "$directory"/*; do
+    if [ -f "$item" ]; then
+        echo "$(basename "$item") is file"
+    elif [ -d "$item" ]; then
+        echo "$(basename "$item") is dir"
+    else
+        echo "$(basename "$item") is not file or dir"
+    fi
+done
